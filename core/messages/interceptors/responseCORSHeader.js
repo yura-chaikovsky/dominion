b54e6cfb77a68c9681c1a73b6f936e15855736d4
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2016-12-18 14:21:58 +0200

    Fix CORS interceptor

--- core/messages/interceptors/responseCORSHeader.js
+++ core/messages/interceptors/responseCORSHeader.js
@@ -6,6 +6,11 @@ Message.response.addInterceptor(responseInterceptorAddCORSHeader);
 function responseInterceptorAddCORSHeader(body) {
     return Promise.resolve().then(() => {
         this.response.headers['Access-Control-Allow-Origin'] = '*';
+        this.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        this.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Set-Cookies';
+        if(this.request.method == 'OPTIONS'){
+            this.response.status = this.response.statuses._200_OK;
+        }
         return body;
     });
 }
