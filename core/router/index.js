29cf47967434fc5b41c89f00c8b3e474028faccd
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2016-12-18 03:51:02 +0200

    Refactor core/router

--- core/router/index.js
+++ core/router/index.js
@@ -1,104 +1,98 @@
-const Messages = require('./../messages');
-const Errors = require('./../errors');
+const Messages                  = use('core/messages');
+const Errors                    = use('core/errors');
+
+
 const registeredRoutes = new Map();
 
-const Router = () => {
-};
-
-
-Router.addRoutes = function (handlersDefinitions) {
-    if (handlersDefinitions == null) {
-        throw new Errors.Fatal('handlersDefinitions is missed');
-    }
-
-    handlersDefinitions.forEach((handler) => {
-        let handlersMap;
-
-        if (!registeredRoutes.has(handler.method)) {
-            registeredRoutes.set(handler.method, new Map());
+module.exports = class Router {
+    static addRoutes (handlersDefinitions) {
+        if (handlersDefinitions == null) {
+            throw new Errors.Fatal('handlersDefinitions is missed');
         }
-        handlersMap = registeredRoutes.get(handler.method);
 
-        if (handler.annotation.path) {
+        handlersDefinitions.forEach((handler) => {
+            let handlersMap;
 
-            let ArrAnnotation = handler.annotation.path;
+            if (!registeredRoutes.has(handler.method)) {
+                registeredRoutes.set(handler.method, new Map());
+            }
+            handlersMap = registeredRoutes.get(handler.method);
 
-            handlersMap.set(new RegExp(`^${ArrAnnotation}$`), handler);
+            if (handler.annotation.path) {
 
-        } else {
+                let ArrAnnotation = handler.annotation.path;
 
-            if (handlersMap.has(handler.arguments.args.pattern)) {
-                throw new Errors.Fatal(`Route for ${handler.method} ${handler.arguments.args.pattern} is already defined`);
+                handlersMap.set(new RegExp(`^${ArrAnnotation}$`), handler);
+
+            } else {
+
+                if (handlersMap.has(handler.arguments.args.pattern)) {
+                    throw new Errors.Fatal(`Route for ${handler.method} ${handler.arguments.args.pattern} is already defined`);
+                }
+
+                handlersMap.set(handler.arguments.args.pattern, handler);
+
             }
 
-            handlersMap.set(handler.arguments.args.pattern, handler);
+        });
 
-        }
-
-    });
-
-    return handlersDefinitions;
-};
-
-
-Router.handle = function (req, res) {
-    const message = new Messages(req, res);
-    const [args, handler] = findRoute(message.request.method, message.request.path);
-
-    let permission;
-    if (handler.annotations && handler.annotations.find(annotation => annotation.key === 'permission').length > 0){
-        permission = handler.annotations.find(annotation => annotation.key === 'permission')[0];
-    }
-    else{
-        permission = handler.permission;
-    }
-
-    message.request.handler = {
-        permission,
-        URLParameters: handler.arguments,
-        annotations: handler.annotations
+        return handlersDefinitions;
     };
 
-    const handlers = [...Messages.request.getInterceptors(),
-        handler.handler.bind(message, ...args),
-        ...Messages.response.getInterceptors()];
+    static handle (req, res) {
+        const message = new Messages(req, res);
+        const [args, handler] = findRoute(message.request.method, message.request.path);
 
-    let messagePromise = Promise.resolve();
-    handlers.forEach(function (interceptorFunction) {
-        messagePromise = messagePromise.then(interceptorFunction.bind(message));
-    });
-    messagePromise
-        .catch(function (error) {
-            if (error instanceof Errors.NotFound) {
-                this.response.status = this.response.statuses._404_NotFound;
-            } else if (error instanceof Errors.BadRequest) {
-                this.response.status = this.response.statuses._400_BadRequest;
-            } else if (error instanceof Errors.Validation){
-                this.response.status = this.response.statuses._400_BadRequest;
-            } else if (error instanceof Errors.Unauthorized) {
-                this.response.status = this.response.statuses._401_Unauthorized;
-            } else if (error instanceof Errors.Forbidden) {
-                this.response.status = this.response.statuses._403_Forbidden;
-            } else if (error instanceof Errors.Database && error.originalError.errno == 1451 /* ER_ROW_IS_REFERENCED_2 */){
-                this.response.status = this.response.statuses._409_Conflict;
-            } else {
-                throw error;
-            }
-            return error;
-        }.bind(message))
-        .catch(function (error) {
-            this.response.status = this.response.statuses._500_InternalServerError;
-            return error;
-        }.bind(message))
-        .then(function (body) {
-            this.response.body = body;
-            this.response.send();
-        }.bind(message));
-};
+        let permission;
+        if (handler.annotations && handler.annotations.find(annotation => annotation.key === 'permission').length > 0){
+            permission = handler.annotations.find(annotation => annotation.key === 'permission')[0];
+        }
+        else{
+            permission = handler.permission;
+        }
 
+        message.request.handler = {
+            permission,
+            URLParameters: handler.arguments,
+            annotations: handler.annotations
+        };
 
-Router.clear = function () {
-    registeredRoutes.clear();
+        const handlers = [...Messages.request.getInterceptors(),
+            handler.handler.bind(message, ...args),
+            ...Messages.response.getInterceptors()];
+
+        let messagePromise = Promise.resolve();
+        handlers.forEach(function (interceptorFunction) {
+            messagePromise = messagePromise.then(interceptorFunction.bind(message));
+        });
+        messagePromise
+            .catch(function (error) {
+                if (error instanceof Errors.NotFound) {
+                    this.response.status = this.response.statuses._404_NotFound;
+                } else if (error instanceof Errors.BadRequest) {
+                    this.response.status = this.response.statuses._400_BadRequest;
+                } else if (error instanceof Errors.Validation){
+                    this.response.status = this.response.statuses._400_BadRequest;
+                } else if (error instanceof Errors.Unauthorized) {
+                    this.response.status = this.response.statuses._401_Unauthorized;
+                } else if (error instanceof Errors.Forbidden) {
+                    this.response.status = this.response.statuses._403_Forbidden;
+                } else if (error instanceof Errors.Database && error.originalError.errno == 1451 /* ER_ROW_IS_REFERENCED_2 */){
+                    this.response.status = this.response.statuses._409_Conflict;
+                } else {
+                    throw error;
+                }
+                return error;
+            }.bind(message))
+            .catch(function (error) {
+                this.response.status = this.response.statuses._500_InternalServerError;
+                return error;
+            }.bind(message))
+            .then(function (body) {
+                this.response.body = body;
+                this.response.send();
+            }.bind(message));
+    };
 };
 
 const findRoute = function (method, path) {
@@ -124,4 +118,3 @@ const findRoute = function (method, path) {
     }];
 };
 
-module.exports = Router;
