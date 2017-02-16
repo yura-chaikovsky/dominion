e78cf617922d12898c4a4afa330982131f45130e
Boris Kotlyarov <9309706@gmail.com>
2017-02-16 13:24:22 +0200

    access-token header has been added to OPTIONS response

--- core/messages/interceptors/responseCORSHeader.js
+++ core/messages/interceptors/responseCORSHeader.js
@@ -7,7 +7,7 @@ function responseInterceptorAddCORSHeader(body) {
     return Promise.resolve().then(() => {
         this.response.headers['Access-Control-Allow-Origin'] = '*';
         this.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
-        this.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Set-Cookies';
+        this.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Set-Cookies, access-token';
         if(this.request.method == 'OPTIONS'){
             this.response.status = this.response.statuses._200_OK;
         }
