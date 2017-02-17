44657125cdde5b16e501614265ff13b9937e211f
Boris Kotlyarov <9309706@gmail.com>
2017-02-17 18:50:04 +0200

    merge headers has been fixed

--- core/messages/interceptors/responseCORSHeader.js
+++ core/messages/interceptors/responseCORSHeader.js
@@ -6,9 +6,7 @@ Message.response.addInterceptor(responseInterceptorAddCORSHeader);
 
 function responseInterceptorAddCORSHeader(body) {
     return Promise.resolve().then(() => {
-        Object.keys(Config.CorsAllowHeaders).forEach((key)=>{
-            this.response.headers[key] = Config.CorsAllowHeaders[key];
-        });
+        Object.assign(this.response.headers, Config.CorsAllowHeaders);
         if(this.request.method == 'OPTIONS'){
             this.response.status = this.response.statuses._200_OK;
         }
