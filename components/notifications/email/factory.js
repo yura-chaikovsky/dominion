1414258c5ed604afb004899efd204508be400e9b
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2017-04-12 21:28:32 +0300

    fix setting config in notification models

--- components/notifications/email/factory.js
+++ components/notifications/email/factory.js
@@ -37,7 +37,7 @@ const NotificationEmailDefinition = {
             newModel.providerConfig = providerConfig;
             newModel.provider_name = newModel.providerConfig.name;
             newModel.provider = require(`./providers/${newModel.provider_name}`);
-            newModel.provider.config(newModel.providerConfig);
+            newModel.provider.config = newModel.providerConfig;
 
             return Promise.resolve(Object.freeze(newModel));
         }
