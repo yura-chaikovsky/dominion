96ec21e199b3404b42db94ed0060a0c9306dde63
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2017-09-14 12:57:37 +0300

    Allow controller to have the same path

--- core/controllers/index.js
+++ core/controllers/index.js
@@ -10,8 +10,6 @@ class Controllers {
     static define (controller){
         if(!controller.path) {
             throw new Errors.Fatal(`Property 'path' is missing in controller`);
-        }else if(controllersCollection.has(controller.path)){
-            throw new Errors.Fatal(`Controller with path '${controller.path}' is already defined`);
         }else{
             controllersCollection.set(controller.path, getHandlersDefinitions(controller));
         }
