eee45854f0a03df2590399cc703ec5108a908b6c
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2017-12-17 21:06:17 +0200

    Remove specs

--- specs/global/globalTestsOfServer.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Global tests', () => {
-    describe('GET', () => {
-        frisby.create('Get status 501 if route doesn\'t exists')
-            .get('/notexisting/route')
-            .expectStatus(501)
-            .toss();
-    });
-});
