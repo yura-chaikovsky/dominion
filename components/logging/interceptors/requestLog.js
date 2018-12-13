8b33479f19b82b89777b335d86bf2c5640023863
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2018-12-13 20:59:37 +0200

    Remove redundant promise

--- components/logging/interceptors/requestLog.js
+++ components/logging/interceptors/requestLog.js
@@ -5,16 +5,12 @@ function requestInterceptorLogging() {
 
     const LogsFactory = Factories('Logs');
 
-    return new Promise((resolve, reject) => {
-        LogsFactory.new({
-                tracking_id: (this.request.tracking && this.request.tracking.id) || null,
-                header: JSON.stringify(this.request.headers),
-                body: JSON.stringify(this.request.body)
-            }, false)
-        .then(log => log.save())
-        .then(resolve)
-        .catch(reject);
-    });
+    return LogsFactory.new({
+        tracking_id: (this.request.tracking && this.request.tracking.id) || null,
+        header: JSON.stringify(this.request.headers),
+        body: JSON.stringify(this.request.body)
+    })
+        .then(log => log.save());
 }
 
 module.exports = requestInterceptorLogging;
