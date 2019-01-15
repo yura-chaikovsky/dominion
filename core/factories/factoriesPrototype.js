06de556c901ec8843ff35e1c24cf9dc6adb8fc1d
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2019-01-15 10:08:37 +0200

    Add fetch with cache method

--- core/factories/factoriesPrototype.js
+++ core/factories/factoriesPrototype.js
@@ -1,6 +1,8 @@
 const Errors                    = require('./../errors');
 
 module.exports = new (function ModelFactory() {
+    this.__cache__ = new Map();
+    this.__cacheDuration__ = 10 * 1000/*ms */;
 
     this.new = function (properties = {}, unsaved = true) {
         let newModel = new this.__model__(properties);
@@ -8,11 +10,11 @@ module.exports = new (function ModelFactory() {
         return Promise.resolve(Object.seal(newModel));
     };
 
-    this.get = function(criteria){
+    this.get = function (criteria) {
         return this.find(criteria, 1, 0).then((models) => {
-            if(models.length){
+            if (models.length) {
                 return models[0];
-            }else{
+            } else {
                 throw new Errors.NotFound(this.__model__.name + ' model by criteria ' + JSON.stringify(criteria) + ' not found');
             }
         });
@@ -20,7 +22,21 @@ module.exports = new (function ModelFactory() {
 
     this.find = function (criteria = {}, limit, offset) {
         return this.repo.find(criteria, limit, offset)
-            .then(rows => Promise.all(rows.map(row => this.new(row, false))) );
+            .then(rows => Promise.all(rows.map(row => this.new(row, false))));
     };
+
+    this.fetch = function (criteria) {
+        const id = Object.values(criteria)[0];
+
+        if (this.__cache__.has(id)) {
+            return Promise.resolve(this.__cache__.get(id)[0]);
+        } else {
+            return this.get(criteria).then(model => {
+                this.__cache__.set(id, [model, new Date()]);
+                setTimeout(() => this.__cache__.delete(id), this.__cacheDuration__);
+                return model;
+            });
+        }
+    }
 
 });
