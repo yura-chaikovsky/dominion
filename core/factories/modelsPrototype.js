5296c91cb81bd75daa4c7b900115995893d80f7c
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2019-01-15 10:19:49 +0200

    Fix formatting

--- core/factories/modelsPrototype.js
+++ core/factories/modelsPrototype.js
@@ -1,5 +1,5 @@
-const PropertyCollection                = use('core/property/collection');
-const Errors                            = use('core/errors');
+const PropertyCollection        = use('core/property/collection');
+const Errors                    = use('core/errors');
 
 module.exports = new (function Model() {
 
@@ -11,15 +11,15 @@ module.exports = new (function Model() {
         return this.repo.remove(this);
     };
 
-    this.toJSON = function(){
+    this.toJSON = function () {
         return PropertyCollection.output(this.__properties__, this.scheme);
     };
 
-    this.populate = function(props){
+    this.populate = function (props) {
         Object.keys(props).forEach((propName) => {
-            if(this.__proto__.hasOwnProperty(propName)){
+            if (this.__proto__.hasOwnProperty(propName)) {
                 this[propName] = props[propName];
-            }else{
+            } else {
                 throw new Errors.Validation(`The object populating model ${this.__name__} does not match its structure, property ${propName} is redundant`)
             }
         });
@@ -27,8 +27,8 @@ module.exports = new (function Model() {
         return this;
     };
 
-    this.validate = function(){
+    this.validate = function () {
         PropertyCollection.validate(this.__properties__, this.scheme, this.__name__);
-    }
+    };
 
 });
